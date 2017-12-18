599255ec9134653be8ea9152bf73581432b2a76d
kiarash <hi@kiarash.info>
2017-12-18 10:11:03 +0330

    Initial Commit

---
+++ LinkList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkList
+//
+//  Created by Kiarash on 12/18/17.
+//  Copyright © 2017 Kiarash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
